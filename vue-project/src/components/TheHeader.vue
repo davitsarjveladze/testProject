c1870958d57cb4308269a28874fdc740e489b018
davitsarjveladze <55948141+davitsarjveladze@users.noreply.github.com>
2022-09-29 15:12:01 +0400

    Feat: add project header Feat: linked fonts Refactor: remove unused code in app.vue

---
+++ vue-project/src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="px-10p py-4 flex items-center main-border">
+    <span class="logo-text font-black mr-20 -"> Project </span>
+    <span class="page-name-text font-bold"> Recruitment Pipeline </span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
